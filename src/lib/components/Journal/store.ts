a0f17851fa245d5183d98abb22f83725951441d7
EdwardGold Edem Hogan <edge37@outlook.com>
2023-02-26 05:19:43 +0100

    ..

--- src/lib/components/Journal/store.ts
+++ src/lib/components/Journal/store.ts
@@ -1,5 +1,5 @@
 import { arrayStore } from "$lib/store";
 import type { Journal, Entry } from "./types";
 
-export const journals = arrayStore<Journal>('journals', []);
-export const entries = arrayStore<Entry>('entries', []);
+export const journals = arrayStore<string>('journals', []);
+export const entries = arrayStore<string>('entries', []);
