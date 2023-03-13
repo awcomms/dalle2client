603074863b913e7cbb3542f23c83cdbf39a2a1d9
EdwardGold Edem Hogan <edge37@outlook.com>
2023-03-13 18:59:20 +0100

    sd

--- src/lib/openai/index.ts
+++ src/lib/openai/index.ts
@@ -1,5 +1,8 @@
+import { OPENAI } from '$env/static/private';
 import { OpenAIApi } from 'openai';
 import { Configuration } from 'openai/dist/configuration';
+
+export const openai = new OpenAIApi(new Configuration({apiKey: OPENAI}))
 
 export const getOpenAI = (apiKey: string) => {
 	return apiKey
