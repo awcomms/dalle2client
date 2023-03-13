557d372025e9b3391b25286f873463841c631e67
EdwardGold Edem Hogan <edge37@outlook.com>
2023-03-13 19:00:00 +0100

    sd

--- src/routes/openai/completions/chat/+server.ts
+++ src/routes/openai/completions/chat/+server.ts
@@ -1,5 +1,7 @@
+import { openai } from "$lib/openai";
 import type { RequestHandler } from "./$types";
 
 export const POST = (() => {
-    
+    openai
+    return new Response(null, {status: 200})
 }) satisfies RequestHandler
