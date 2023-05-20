50a104046b48f8ddb5a616e195664fbe94be7cbd
EdwardGold Edem Hogan <edge37@outlook.com>
2023-05-20 15:38:10 +0100

    ..

--- src/hooks/server.ts
+++ src/hooks/server.ts
@@ -1,7 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import AzureADB2C from "@auth/core/providers/azure-ad-b2c"
-import { AZUREADB2C_ID, AZUREADB2C_SECRET } from "$env/static/private";
+// import { AZUREADB2C_ID, AZUREADB2C_SECRET } from "$env/static/private";
 
-export const handle = SvelteKitAuth({
-    providers: [AzureADB2C({clientId: AZUREADB2C_ID, clientSecret: AZUREADB2C_SECRET})]
-})
+// export const handle = SvelteKitAuth({
+//     providers: [AzureADB2C({clientId: AZUREADB2C_ID, clientSecret: AZUREADB2C_SECRET})]
+// })
