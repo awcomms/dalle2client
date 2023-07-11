662aab31c4f737d4aa8571b66bd69283e3a9ddba
EdwardGold Edem Hogan <edge37@outlook.com>
2023-07-11 14:17:12 +0000

    .

--- src/lib/components/ai_chat/Interface.svelte
+++ src/lib/components/ai_chat/Interface.svelte
@@ -6,7 +6,7 @@
 		name_label = 'Name',
 		parameters: CreateChatCompletionRequest =
 			{
-				model: 'gpt-3.5-turbo-16k',
+				model: 'gpt-4',
 				temperature: 1,
 				top_p: 1,
 				messages: [],
