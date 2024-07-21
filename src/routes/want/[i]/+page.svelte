ad9496bc7471458d6c2fc07324f36300e37e292d
Gold Edem Hogan <edge37@outlook.com>
2024-07-21 09:29:56 +0100

    .

--- src/routes/want/[i]/+page.svelte
+++
@@ -1,3 +0,0 @@
-<script>
-    export i: string
-</script>
--- src/routes/want/[i]/+server.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "$lib/util/redis"
-
-export const load: PageServerLoad = ({params}) => {
-    return {
-        client.json.get(params.i)
-    }
-}
