b9de2cd8bfb8074808526b740a151cf9716ae137
Gold Edem Hogan <edge37@outlook.com>
2024-08-03 13:44:14 +0100

    .

--- src/lib/util/cors/get_cors_headers.ts
+++ src/lib/util/cors/get_cors_headers.ts
@@ -1,11 +1,13 @@
+import { origin_allowed } from './origin_allowed';
+
 export const get_cors_headers = (origin: string | null): Record<string, string> => {
-	// if (isAllowedOrigin(origin)) {
-	return {
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-		'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-		// Vary: 'Origin'
-	};
-	// }
-	// return {};
+	if (origin_allowed(origin)) {
+		return {
+			'Access-Control-Allow-Origin': origin || '*',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+			Vary: 'Origin'
+		};
+	}
+	return {};
 };
